fa12a6e0fa8567441659ea045f254a4401103878
Nicolas Feldfeber <nicofeldfeberdesarrollador@gmail.com>
2019-10-21 22:16:08 -0300

    Base Model created and migrated

--- to_do_list_app/models.py
+++ to_do_list_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class List(models.Model):
+    title = models.CharField(max_length=300)
+    last_update = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField()
+
+
+class Item(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE, verbose_name="List")
+    text = models.CharField(max_length=500, verbose_name="Text")
+    checked = models.BooleanField(default=False, verbose_name="Checked")
+    checked_date = models.DateTimeField(default=None, blank=True)
+    creation_date = models.DateTimeField()
